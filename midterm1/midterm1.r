7c4a1f757ddefd996050ce67d2a9df1b0ffd5927
Shashank Bhargava <sbhargava@berkeley.edu>
2015-03-13 14:01:08 -0700

    midterm done

--- midterm1/midterm1.r
+++ midterm1/midterm1.r
@@ -1,22 +1,23 @@
 # Please load in the dataset included in the midterm1 directory (SFHousing-2.rda). 
 # It will be required to perform the following tasks. The dataset includes data for houses
 # in the city of Berkeley.
-
+load("SFHousing-2.rda")
+ 
 # calculate the mean lot size (lsqft) of houses in Berkeley. Store it as the
 # variables <mean.lsqft>.
 
-# mean.lsqft <- your code here
+mean.lsqft <- mean(housing$lsqft, na.rm = TRUE)
 
 # How many unique area codes are there in the dataset?  Store them in the variable
 # <n.zipcode>
 
-# n.zipcode <- your code here
+n.zipcode <- unique(housing$zip)
 
 # For each house in the dataset, calculate how large the house is relative to
 # the lot size, i.e. square foot of house over square foot of lot.
 # Store it in the variable <rel.sqft>.
 
-# rel.sqft <- your code here
+rel.sqft <- with(housing, bsqft/lsqft)
 
 
 # Please create two new data frames with the following two subsets
@@ -24,15 +25,16 @@
 # 1) houses whose bsqft is strictly greater than <mean.bsqft>:  <bsft.greater>
 # 2) houses whose bsqft is less than or equal to  <mean.bsqft>: <bsqft.less>
 
-# bsqft.greater <- your code here
-# bsqft.less <- your code here
+mean.bsqft <- mean(housing$bsqft, na.rm = TRUE)
+bsqft.greater <- housing[which(housing$bsqft>mean.bsqft),]
+bsqft.less <- housing[which(housing$bsqft<=mean.bsqft),]
 
 
 # For each of your subsets, create a vector giving the price of each house. Name
 # these variables <rooms.greater.price> and <rooms.less.price>.
 
-# rooms.greater.price <- your code here
-# rooms.less.price <- your code here
+rooms.greater.price <- bsqft.greater$price
+rooms.less.price <- bsqft.less$price
 
 
 
@@ -49,9 +51,15 @@
 # <br> in the range (inclusive) specified by <room.range>
 
 priceByRooms <- function(room.range, br, prices) {
-
-    # your code here
-
+  total = 0
+  observations <- data.frame(br, prices)
+  for(i in 1:length(observations$br)){
+    if(observations$br[i] >= room.range[1] & observations$br[i] <= room.range[2]){
+      total<- total + observations$prices[i]
+    }
+  }
+  housing.average <- total/length(observations$br)
+  return(housing.average)
 }
 
 
@@ -61,3 +69,4 @@ priceByRooms <- function(room.range, br, prices) {
 # 2) axis labels: "Price" and "#rooms"
 # 3) plotting character set to 19
 
+plot(housing$br, housing$price, main= "Housing price vs Number of Rooms", xlab = "#Rooms", ylab="Price", pch=19)
