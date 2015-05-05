b1a9244b5ecc7eee2dbc9bc862d5be8f30985a96
Shashank Bhargava <sbhargava@berkeley.edu>
2015-05-05 15:28:24 -0700

    made more stuff happen

--- coolthings.r
+++ coolthings.r
@@ -1,5 +1,9 @@
 #if you have a logical expression within square brackets it will return the stuff only where its true!
 with apply, 1 means yes, 2 means no
+apply is used on arrays and matrix
+lapply is for lists
+sapply is for simplifying shit
+tapply is for tables
 look into replicate function
 grep gets you locations of the first argument with the second argument becing the searched vector
 gregexpr tells you the locations of the matches
